6f1a6a9fc67a6b0c116e6fd981f4631f68762142
Nathan Newyen <nathan@nathannewyen.com>
2024-04-29 02:10:31 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "sounds/explode.wav");
+    audio.add("lose", "sounds/lose.wav");
+    audio.add("move", "sounds/move.wav");
+    audio.add("pew", "sounds/pew.wav");
+    audio.add("startup", "sounds/startup.wav");
+    audio.add("win", "sounds/win.wav");
+    audio.play("startup");
+
+    // Cleanup
+    audio.wait();
+    Ok(())
+}
