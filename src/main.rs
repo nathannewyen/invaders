3b5b43c14cc3bff4834d7ffa5e3e3692112abbd3
Nathan Newyen <nathan@nathannewyen.com>
2024-04-29 11:44:27 -0400

    add function escape game and rendering color for game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use std::error::Error;
+use std::io;
+use std::time::Duration;
+use crossterm::{event, ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
 
 fn main() -> Result <(), Box<dyn Error>> {
@@ -11,7 +17,32 @@ fn main() -> Result <(), Box<dyn Error>> {
     audio.add("win", "sounds/win.wav");
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game loop
+    'game: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'game;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
     // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
